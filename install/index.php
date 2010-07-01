f8079b21dd35d596edd300bbc51d56a2e2f3c7f7
Alexandre SIMON <alexandre.simon@rbs.fr>
2010-07-01 11:29:22 +0200

    empêcher les pages d'êtres cachées

--- install/index.php
+++ install/index.php
@@ -1,4 +1,9 @@
 <?php
+header('Last-Modified' .': '. gmdate('D, d M Y H:i:s') . ' GMT');
+header('Expires' .': '. 'Mon, 26 Jul 1997 05:00:00 GMT');
+header('Cache-Control' .': '. 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+header('Pragma' .': '. 'no-cache');
+
 define('PROJECT_HOME_PATH', dirname(dirname(__FILE__)));
 include PROJECT_HOME_PATH . '/install/lib/check_install.php';
 $step = isset($_POST['NEXTSTEP']) ? $_POST['NEXTSTEP'] : '01';
