ef9d5095954270f63d93cb75a61a8e38bea6e185
Alexandre SIMON <alexandre.simon@rbs.fr>
2010-07-01 11:28:23 +0200

    [NEW] test du dossier d'installation

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-header('Location: /install/index.php');
+$requestURI = ($_SERVER['REQUEST_URI'] == "/") ? "/index.php" : $_SERVER["REQUEST_URI"];
+if ($requestURI != '/'.basename($_SERVER['SCRIPT_NAME']))
+{
+	echo "Dossier d'installation invalide : le script d'installation doit être placé à la racine";
+}
+else
+{
+	header('Location: /install/index.php');
+}
 die();
