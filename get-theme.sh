2cc5a1cb4020c72b78f0c9eea56d18536af555cf
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-04-19 10:35:48 +0200

    [NEW] Mise à jour version 3.5.0

--- get-theme.sh
+++ get-theme.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
+rm -rf themes 2>/dev/null
 mkdir themes/ 2>/dev/null
-wget http://rbschange.fr/static/themes/webfactory-1.0.zip
-unzip webfactory-1.0.zip -d themes/
-rm webfactory-1.0.zip
+git clone  ssh://git@rd.devlinux.france.rbs.fr/home/git/repository/themes.webfactory.git themes/webfactory -b 3.5
+rm -rf themes/webfactory/.git 2>/dev/null
